aea2b968cde5e367cfb625f6a07727802b303ce0
Suguru Oho <oho.sugu@gmail.com>
2025-08-26 11:13:36 +0900

    wip mute

---
+++ mute.go
@@ -0,0 +1,33 @@
+package twitterscraper
+
+import (
+	"context"
+	"net/url"
+	"encoding/json"
+	"bytes"
+	"io"
+)
+
+func (s *Scraper) MuteUser(userId string) error {
+	req, err := s.newRequest("POST", "https://x.com/i/api/1.1/mutes/users/create.json")
+	if err != nil {
+		return err
+	}
+
+	req.Header.Set("content-type", "application/x-www-form-urlencoded")
+
+	strBody := "user_id="+userId
+
+	req.Body = io.NopCloser(bytes.NewReader([]byte(strBody)))
+
+	var response struct {
+	}
+
+	err = s.RequestAPI(req, &response)
+	if err != nil {
+		return err
+	}
+
+	return nil
+}
+
